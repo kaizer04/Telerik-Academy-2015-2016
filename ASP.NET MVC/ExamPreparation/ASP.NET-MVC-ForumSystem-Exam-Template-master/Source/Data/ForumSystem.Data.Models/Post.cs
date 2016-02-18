75472c85332aa0a879685ed87c286e5c79e56df4
kaizer04 <kaizer0505@gmail.com>
2016-02-18 09:42:07 +0200

    Initial commit

---
+++ ASP.NET MVC/ExamPreparation/ASP.NET-MVC-ForumSystem-Exam-Template-master/Source/Data/ForumSystem.Data.Models/Post.cs
@@ -0,0 +1,25 @@
+﻿namespace ForumSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using ForumSystem.Data.Common.Models;
+
+    public class Post : AuditInfo, IDeletableEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        public string AuthorId { get; set; }
+
+        public virtual ApplicationUser Author { get; set; }
+ 
+        public string Content { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ ASP.NET MVC/ExamPreparation/ASP.NET-MVC-ForumSystem-Exam-Template-master/Source/Data/ForumSystem.Data.Models/Tag.cs
@@ -0,0 +1,21 @@
+﻿namespace ForumSystem.Data.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    using ForumSystem.Data.Common.Models;
+
+    public class Tag : AuditInfo, IDeletableEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Index]
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
